e2e75fe0cbb3d503abd7891e56ef7ebd350691a1
Christian Schlotter <again@gmx.de>
2020-01-11 17:04:32 +0100

    Add automatic update script.

---
+++ subsurface/update.ps1
@@ -0,0 +1,35 @@
+import-module au
+
+$releases    = 'https://subsurface-divelog.org/downloads/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*[$]packageName\s*=\s*)('.*')"= "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*[$]fileType\s*=\s*)('.*')"   = "`$1'$($Latest.FileType)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
+function global:au_GetLatest {
+    $releasesPage = Invoke-WebRequest -Uri $releases
+
+    $re  = "subsurface-.*\.exe"
+    $url = $releasesPage.Links | Where-Object href -Match $re | Select-Object -Last 1 -ExpandProperty href
+    $url = $releases + $url
+
+    $version = $url -Split '-|.exe' | Select-Object -Last 1 -Skip 1
+
+    return @{
+        URL32        = $url
+        Version      = $version
+    }
+}
+
+Update-Package -NoReadme -ChecksumFor all
