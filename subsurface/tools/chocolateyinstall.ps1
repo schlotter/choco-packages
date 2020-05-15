a323939695d86beca150c8921031d0ea88a8c378
Christian Schlotter <again@gmx.de>
2020-05-15 08:27:45 +0200

    Simplify chocolateyinstall.ps1 by removing unnecessary variables.

--- subsurface/tools/chocolateyinstall.ps1
+++ subsurface/tools/chocolateyinstall.ps1
@@ -2,22 +2,17 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://subsurface-divelog.org/downloads/subsurface-4.9.3.exe'
-$url64      = ''
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-  url64bit      = $url64
-  file          = $fileLocation
 
   softwareName  = 'subsurface*'
 
   checksum      = '5FE40200F42865C04E8930458BBB1771D06872CFB7C0DDF2FDE40E11AABCA0C6'
   checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'sha256'
 
   silentArgs    = '/S'
   validExitCodes= @(0)
