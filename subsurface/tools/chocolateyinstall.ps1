8faf8dcf6c895435415812d4089eaddbd3dcdf3b
Christian Schlotter <again@gmx.de>
2020-06-21 22:35:38 +0200

    Update subsurface package to version 4.9.6.

--- subsurface/tools/chocolateyinstall.ps1
+++ subsurface/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://subsurface-divelog.org/downloads/subsurface-4.9.5.exe'
+$url        = 'https://subsurface-divelog.org/downloads/subsurface-4.9.6.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'subsurface*'
 
-  checksum      = 'D802C253808C1F14E07D853841CCF156092F4ED625726C6F39CDBEAE92BC30E2'
+  checksum      = 'BF307A158897515B92C09E59BBDC52FC7C414A00FEB2EA35E22BBFE9588A6A31'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
