6fd4c62a11dcd99344e633821b55ef586b107e67
Christian Schlotter <schlotter@users.noreply.github.com>
2021-07-04 09:19:05 +0200

    Update subsurface package to version 5.0.2.

--- subsurface/tools/chocolateyinstall.ps1
+++ subsurface/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://subsurface-divelog.org/downloads/subsurface-5.0.1.exe'
+$url        = 'https://subsurface-divelog.org/downloads/subsurface-5.0.2.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'subsurface*'
 
-  checksum      = '9B9809A6EE43AA77EAC551C08DC001C77FA71202C8FD5EB5594EE644A0EE734D'
+  checksum      = 'A6F6122998402CAE0FD7167D4E4A9510C7EB5218C2015412D0E44E2FA53D6D96'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
