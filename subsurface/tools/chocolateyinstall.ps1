d931eb9c2846386b10b11c3384f02f637ebec8f0
Christian Schlotter <schlotter@users.noreply.github.com>
2025-08-09 12:20:40 +0200

    Update subsurface package to version 6.0.5404.

--- subsurface/tools/chocolateyinstall.ps1
+++ subsurface/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://subsurface-divelog.org/downloads/subsurface-6.0.5365-CICD-release-installer.exe'
+$url        = 'https://subsurface-divelog.org/downloads/subsurface-6.0.5404-CICD-release-installer.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'subsurface*'
 
-  checksum      = '0B0C6DA91D7635D9BD265F505C03E7AF660705F1F70D0B2CB85D0EA6E035513A'
+  checksum      = 'BB47EF978BE047BEB1E718ACB6BD94FAA6A1FB9D0D64EEEB4E89646507D19808'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
